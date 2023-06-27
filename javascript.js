495a248e7ed581d59a8fddbcbba17df2a05f6046
unknown <elhadjoumartall10@gmail.com>
2023-06-27 15:27:13 +0200

    Cours en JS

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-alert("Hi !");
+//document.write("Hello world "); // Permet d'écrire une phrase
+
+
+
+//console.log("problème !"); //pour afficher un message normale
+console.error("Problème !"); //Afficher un message d'erreur 
+//console.clear(); // Pour supprimer le message
+
+//Declarer une variable avec JS 
+// Pour declarer une variable il faut utiliser la fonction var
+//typeof : permet d'afficher le type du caractère
+var text = 15;
+text = 18 ;
+document.write(typeof text);
