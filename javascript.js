0bc5679a1b7464fd3c86d5137b913868ea1c377d
unknown <elhadjoumartall10@gmail.com>
2023-06-27 15:50:09 +0200

    Cours de JS

--- javascript.js
+++ javascript.js
@@ -8,7 +8,23 @@ console.error("Problème !"); //Afficher un message d'erreur
 
 //Declarer une variable avec JS 
 // Pour declarer une variable il faut utiliser la fonction var
+
+
 //typeof : permet d'afficher le type du caractère
-var text = 15;
-text = 18 ;
-document.write(typeof text);
+// var text = 15;
+// text = 18 ;
+// document.write(typeof text);
+
+//La fonction prompt : permet de verifier des informations et les afficher directement 
+// var name = prompt("Bonjour quel est votre nom");
+// document.write(name);
+
+/* Les types de fonctions 
+document.write() afficher les documents 
+alert() ou confirm() afficher un message d'alerte
+typeof() afficher le type de caractère
+prompt() Verifie les informations et les affiches directements 
+parseint()
+parsefloat() Afficher en mode number
+const() c'est une variable constante qu'on ne peut pas modifier
+*/
